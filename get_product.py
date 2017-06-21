f96097f0675c4434dc5a86120671b1fc6ed88c96
satoken912 <seat.soil.74196901@gmail.com>
2017-06-21 18:06:56 +0900

    Initial commit

---
+++ get_product.py
@@ -0,0 +1,21 @@
+# coding: utf-8
+
+import json
+import requests
+from requests_oauthlib import OAuth1
+from oauth_keys import *
+
+
+auth = OAuth1(consumer_key, consumer_secret, access_token, access_secret)
+endpoint = "https://api.zaim.net/v2/home/money"
+
+r = requests.get(endpoint, auth=auth)
+data = json.loads(r.text)
+
+purchases = data["money"]
+for purchase in purchases:
+    category_id = purchase["category_id"]
+    comment = str(purchase["comment"])
+
+    if len(comment) > 0 and category_id == 108:
+        print(category_id, comment)
